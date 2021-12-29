d697db59f8be1df4e1e97564621f2da4dc585490
RAHUL-R10 <85721390+RAHUL-R10@users.noreply.github.com>
2021-12-29 17:11:00 +0530

    Update most_frequency.py

--- most_frequency.py
+++ most_frequency.py
@@ -3,12 +3,12 @@
 from collections import Counter
   
 # initializing string 
-test_str = "GeeksforGeeks"
+test_str = "MISSISSIPPI"
   
 # using collections.Counter() to get 
 # count of each element in string 
 res = Counter(test_str)
   
 # printing result 
-print ("Count of all characters in GeeksforGeeks is :\n "
+print ("Count of all characters in MISSISSIPPI is :\n "
                                            +  str(res))
